4be116c6996bd2075211eeb3ab6deed8e52a722c
Theo Brown <7982453+theo-brown@users.noreply.github.com>
2020-09-14 19:02:18 +0100

    Update first-time-laptop-setup.sh

--- first-time-laptop-setup.sh
+++ first-time-laptop-setup.sh
@@ -2,6 +2,6 @@ sudo apt update
 sudo apt upgrade
 sudo apt install libc6-dev
 sudo ubuntu-drivers autoinstall
-sudo apt install steam gimp skypeforlinux spyder codeblocks easytag
+sudo apt install steam gimp skypeforlinux spyder codeblocks easytag librecad python3-pip
 sudo apt purge hexchat transmission
 sudo apt autoremove
