5b586f011451ff06865d0dedb56909d9e1d706dc
Theo Brown <7982453+theo-brown@users.noreply.github.com>
2020-09-14 18:59:51 +0100

    Create first-time-laptop-setup.sh

---
+++ first-time-laptop-setup.sh
@@ -0,0 +1,7 @@
+sudo apt update
+sudo apt upgrade
+sudo apt install libc6-dev
+sudo ubuntu-drivers autoinstall
+sudo apt install steam gimp skypeforlinux spyder codeblocks easytag
+sudo apt purge hexchat transmission
+sudo apt autoremove
